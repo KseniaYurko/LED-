f9b6e39f07a4b6ae08a51fb0feef7c366fda0b59
Юрко Ксения <iurko.kk@phystech.edu>
2021-03-27 23:36:36 +0300

    Везде добавила ввод

--- Arduino-python LED/3-runningLight.py
+++ Arduino-python LED/3-runningLight.py
@@ -5,15 +5,15 @@ board = pyfirmata.Arduino("COM4")
 N = [2,3,4,5,6,7,8,9]
 
 def runningLight(count, period):
-    
     for i in range(count):
-        board.digital[N[0]].write(1)
+        board.digital[N[ledNumber]].write(1)
         time.sleep(period)
-        board.digital[N[0]].write(0)
-        if N[0]<9:
-            N[0]=N[0]+1
-        elif N[0]>=9:
-            N[0]=N[0]-7
+        board.digital[N[ledNumber]].write(0)
+        if N[ledNumber]<9:
+            N[ledNumber]=N[ledNumber]+1
+        elif N[ledNumber]>=9:
+            N[ledNumber]=N[ledNumber]-7
+ledNumber = int(input('С какого светодиода начинать? '))
 count = int(input('Сколько раз переключить светодиод? '))
 period = float(input('Интервал: '))
 runningLight(count, period)
--- Arduino-python LED/4-runningDark.py
+++ Arduino-python LED/4-runningDark.py
@@ -8,11 +8,14 @@ def runningDark(count, period):
     for i in range (8):
         board.digital[N[i]].write(1)
     for i in range(count):
-        board.digital[N[0]].write(0)
+        board.digital[N[ledNumber]].write(0)
         time.sleep(period)
-        board.digital[N[0]].write(1)
-        if N[0]<9:
-            N[0]=N[0]+1
-        elif N[0]>=9:
-            N[0]=N[0]-7
-runningDark(300, 0.2) 
+        board.digital[N[ledNumber]].write(1)
+        if N[ledNumber]<9:
+            N[ledNumber]=N[ledNumber]+1
+        elif N[ledNumber]>=9:
+            N[ledNumber]=N[ledNumber]-7
+ledNumber = int(input('С какого светодиода начинать? '))
+count = int(input('Сколько раз переключить светодиод? '))
+period = float(input('Интервал: '))
+runningDark(count, period)
