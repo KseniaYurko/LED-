b090640f47999ad175a533db9354bd312f9a7075
Юрко Ксения <iurko.kk@phystech.edu>
2021-03-30 12:31:58 +0300

    FINAL2

--- Arduino-python LED/FINAL.py
+++ Arduino-python LED/FINAL.py
@@ -4,9 +4,6 @@ import time
 board = pyfirmata.Arduino("COM4")
 N = [2,3,4,5,6,7,8,9]
 
-def closeAll(all):
-    for i in range (len(N)):
-        board.digital[N[i]].write(0)
 
 print ('ЗДАДАНИЕ №1 - lightUp')
 print ('Свечение заданной лампочки заданное время.')
@@ -37,7 +34,6 @@ ledNumber = int(input('Введи номер светодиода: '))
 blinkCount = int(input('Количество миганий: '))
 blinkPeriod = float(input('Интервал: '))
 blink(ledNumber, blinkCount, blinkPeriod)
-closeAll(all)
 print()
 
 
@@ -53,27 +49,30 @@ def runningLight(count, period):
 ledNumber = int(input('С какого светодиода начинать? '))
 count = int(input('Сколько раз переключить светодиод? '))
 period = float(input('Интервал: '))
-closeAll(all)
 runningLight(count, period)
+for i in range (8):
+    board.digital[N[i]].write(0)
 print()
 
 
 print ('ЗДАДАНИЕ №4 - runningDark')
 print ('"Бегущий огонек" с заданного номера светодиода')
 def runningDark(count, period):
-    for i in range (8):
+    for i in range (len(N)):
         board.digital[N[i]].write(1)
     for i in range(count):
-        lightDown(ledNumber, period)
+        board.digital[N[ledNumber]].write(0)
+        time.sleep(period)
+        board.digital[N[ledNumber]].write(1)
         if N[ledNumber]<9:
             N[ledNumber]=N[ledNumber]+1
         elif N[ledNumber]>=9:
             N[ledNumber]=N[ledNumber]-7
+    
 ledNumber = int(input('С какого светодиода начинать? '))
 count = int(input('Сколько раз переключить светодиод? '))
 period = float(input('Интервал: '))
 runningDark(count, period)
-closeAll(all)
 print()
 
 
@@ -86,9 +85,15 @@ def decToBinList(decNumber):
     for i in range (8-c):
         number.insert(i, '0')
     print(number)
+for i in range (8):
+    board.digital[N[i]].write(0)
 decNumber = int(input('Введите число от 0 до 255: '))
+
 decToBinList(decNumber)
+for i in range (8):
+    board.digital[N[i]].write(0)
 print()
+
 
 
 print ('ЗДАДАНИЕ №6 - lightNumber')
@@ -110,7 +115,6 @@ def lightNumber(number):
 decNumber = int(input('Введите число от 0 до 255: '))
 lightNumber(decNumber)
 print()
-
 
 print ('ЗДАДАНИЕ №7 - runningPattern')
 print ('"Бегущий узор" заданное число раз')
@@ -148,4 +152,5 @@ count = int(input('Сколько раз передвинуть? '))
 period = float(input('Интервал: '))
 direction = int(input('В какую сторону? (1-вправо; 0-влево) '))
 runningPattern(pattern, direction)
-closeAll(all)
+for i in range (8):
+    board.digital[N[i]].write(0)
